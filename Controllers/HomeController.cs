7ba75070a4a38238736f2c1031eb0ca7a5aac5a7
Leonardo <leonardo.f.silva35@aluno.senai.br>
2026-05-29 16:56:05 -0300

    bibliotec 3.0

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bibliotec_MVC_DEV.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
